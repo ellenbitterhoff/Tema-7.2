26d088666be855330e70a44575bd5154b3b4627c
ellenbitterhoff <ellen.bitterhoff@gmail.com>
2023-02-07 11:21:47 +0100

    temp

--- 07.01.02webshop/produktliste.js
+++ 07.01.02webshop/produktliste.js
@@ -11,7 +11,7 @@ function menutoggle() {
 /*https://kea-alt-del.dk/t7/api/products/*/
 async function hentData() {
   const response = await fetch(
-    "https://kea-alt-del.dk/t7/api/products?brand=Nike?limit=10"
+    "https://kea-alt-del.dk/t7/api/products?limit=30 "
   );
   const data = await response.json();
   console.log(data);
@@ -25,11 +25,25 @@ function visProdukt(produkt) {
   console.log(template);
   const copy = template.cloneNode(true);
   copy.querySelector("h3").textContent = produkt.productdisplayname;
+  copy.querySelector(".category").textContent = produkt.category;
+  copy.querySelector(".price").textContent = produkt.price;
+  copy.querySelector(".type").textContent = produkt.brandname;
+
+  copy.querySelector("a").href = "products.html?id=" + produkt.id;
+  copy.querySelector(
+    ".img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+
   if (produkt.soldout) {
     copy.querySelector("article").classList.add("soldOut");
   }
   if (produkt.discount) {
     copy.querySelector("article").classList.add("onSale");
+    const discountPrice =
+      produkt.price - produkt.price * (produkt.discount / 100);
+    copy.querySelector(".newprice").textContent = discountPrice;
   }
   document.querySelector(".grid_1").appendChild(copy);
 }
+//
+hentData();
